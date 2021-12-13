35071924817c423d18d2ea7dd46ec2c72f52a446
shashank <shashank.omar@luezoid.com>
2021-12-13 16:58:36 +0530

    project dependencies updated

--- app/src/main/java/com/e/paginatorkotlindemo/activity/PaginationActivity.kt
+++ app/src/main/java/com/e/paginatorkotlindemo/activity/PaginationActivity.kt
@@ -24,6 +24,7 @@ class PaginationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_pagination)
 
         setRecyclerView()
